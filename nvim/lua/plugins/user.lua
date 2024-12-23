1baf0a6807ba5f7291d2d670e96bc6ba05f788fe
Mandeep Singh <sonomandeep@gmail.com>
2024-12-23 14:24:38 +0000

    update astronvim config

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -1,3 +1,5 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
@@ -38,7 +40,7 @@ return {
   },
 
   -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = true },
+  { "max397574/better-escape.nvim", enabled = false },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
