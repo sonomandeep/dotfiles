5de0cfe98c706103523262a8b6ee67ada9562aae
Mandeep Singh <sonomandeep@gmail.com>
2026-08-19 17:27:46 +0200

    feat: update to ayu theme

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+
+vim.keymap.set("n", "<leader>tt", function()
+  vim.o.background = vim.o.background == "dark" and "light" or "dark"
+end, { desc = "Toggle theme" })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,10 @@
 return {
   {
-    "xiyaowong/transparent.nvim",
-    lazy = false,
+    "Shatur/neovim-ayu",
+    priority = 1000,
     config = function()
-      require("transparent").setup({
-        extra_groups = {
-          "NormalFloat", -- Floating windows
-          "NvimTreeNormal", -- File explorer
-          "NeoTreeNormal", -- Neo-tree
-          "StatusLine", -- Status line background
-          "TabLineFill", -- Tab bar background
-          "CursorLine", -- Line highlight
-        },
-      })
-      -- Auto-enable transparency on startup
-      vim.cmd("TransparentEnable")
+      require("ayu").setup({ mirage = false })
+      vim.cmd("colorscheme ayu")
     end,
-  },
-  -- Ensure the colorscheme doesn't overwrite transparency
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight", -- Or your preferred Bauhaus-like scheme
-    },
   },
 }
